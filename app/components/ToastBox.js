798ea1222178ea37208993a50fff8a62d6d81ad4
zhouyuexie <zhouyuexie@gmail.com>
2016-11-07 21:14:59 +0800

    使用到首页下拉更新后提示更新的文章条数,之前没有启用这个组件

--- app/components/ToastBox.js
+++ app/components/ToastBox.js
@@ -10,6 +10,7 @@ import {
 	PixelRatio,
 	TouchableOpacity,
 	Platform,
+	LayoutAnimation,
 	Animated
 } from 'react-native';
 import Reactotron from 'reactotron-react-native';
@@ -19,105 +20,63 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 
 class ToastBox extends Component{
 	state = {
-		TopX:new Animated.Value(0),
 		TopY:new Animated.Value(0),
+		DownY:new Animated.Value(0),
+		height:0
 	}
 	render(){
 		return(
-			<View style={styles.container}>
+			<View style={[styles.container,{height:this.state.height}]}>
 				<Animated.View style={[
-					styles.body,{
-					transform: [{
-					 translateY: this.state.TopX.interpolate({
-						 inputRange: [0, 1],
-						 outputRange: [0, Width/2]
-					 })
-				 },{
-					 translateY: this.state.TopY.interpolate({
-						 inputRange: [0, 1],
-						 outputRange: [0, -Width/2]
-					 })
-				 }
-				 ]
-				}]}>
-					<View>
-						<Text>我是飞翔的字体</Text>
-					</View>
+					styles.body,
+					// {
+					// transform: [{
+					//  translateY: this.state.TopY.interpolate({
+					// 	 inputRange: [0, 1],
+					// 	 outputRange: [0, 20]
+					//  })
+				 // },{
+					//  translateY: this.state.DownY.interpolate({
+					// 	 inputRange: [0, 1],
+					// 	 outputRange: [0, -20]
+					//  })
+				 // }
+				 // ]
+				// }
+				]}>
+					<Text>刷新成功,更新15条数据</Text>
 				</Animated.View>
 			</View>
 		)
 	}
-	_Start(){
-		this.state.Atop.setValue(0);
-		Animated.parallel([
-			Animated.spring(
-				this.state.Atop,
-				{toValue: 1,duration: 1000,friction:6,tension:60},
-			),
-			Animated.spring(this.state.Scale,{
-				toValue:1,
-				duration:1000
-			}),
-			Animated.timing(this.state.rotation0,{
-				toValue:1,duration:250,delay:0
-			}),
-			Animated.timing(this.state.rotation1,{
-				toValue:1,duration:250,delay:50
-			}),
-			Animated.timing(this.state.rotation2,{
-				toValue:1,duration:250,delay:100
-			}),
-			Animated.timing(this.state.rotation3,{
-				toValue:1,duration:250,delay:150
-			}),
-		]).start(()=>{
-			this.state.rotation0.setValue(0);
-			this.state.rotation1.setValue(0);
-			this.state.rotation2.setValue(0);
-			this.state.rotation3.setValue(0);
-		})
+	Start(){
+		// this.state.TopY.setValue(0);
 		
-		this.state.Abottom.setValue(0);
-		// this.state.Lines1Top.setValue(0);
-		// this.state.Lines2Top.setValue(0);
-		// Animated.sequence([
-		// 	Animated.spring(this.state.Lines2Top,{
-		// 		toValue: 1,duration: 100,friction:6,tension:60
-		// 	}),
-		// 	Animated.spring(this.state.Lines1Top,{
-		// 		toValue: 1,duration: 100,friction:6,tension:60
-		// 	})
-		// ]).start()
+		// Animated.timing(this.state.TopY,{
+		// 	toValue:1,duration:250,delay:150
+		// }).start();
+		LayoutAnimation.configureNext(LayoutAnimation.Presets.linear);
+		this.setState({
+			height:30
+		})
 	}
-	_End(){
-		this.state.Abottom.setValue(0);
-		Animated.parallel([
-			Animated.spring(
-				this.state.Abottom,
-				{toValue: 1,duration: 1000,friction:6},
-			),
-			Animated.spring(this.state.Scale,{
-				toValue:0,
-				duration:1000
-			})
-		]).start(()=>{
-			this.state.rotation.setValue(0);
-		});
-		// this.state.Lines1Bottom.setValue(0);
-		// this.state.Lines2Bottom.setValue(0);
-		// Animated.sequence([
-		// 	Animated.spring(this.state.Lines1Bottom,{
-		// 		toValue: 1,duration: 500,friction:6,tension:60
-		// 	}),
-		// 	Animated.spring(this.state.Lines2Bottom,{
-		// 		toValue: 1,duration: 500,friction:6,tension:60
-		// 	})
-		// ]).start()
+	End(){
+		// this.state.DownY.setValue(0);
+
+		// Animated.timing(this.state.DownY,{
+		// 	toValue:1,duration:250,delay:150
+		// }).start();
+		LayoutAnimation.configureNext(LayoutAnimation.Presets.linear);
+		this.setState({
+			height:0
+		})
 	}
-	// shouldComponentUpdate(){
-	// 	console.log(this.props.updateAlpha);
-	// 	return true;
-	// }
+	SetTime(time){
+		this.Start();
+		setTimeout(()=>{
+			this.End();
+		},time);
+	}
 }
 
 const styles = StyleSheet.create({
@@ -125,35 +84,14 @@ const styles = StyleSheet.create({
 		flexDirection:"column",
 		alignItems:"center",
 		justifyContent:"center",
-		backgroundColor:"rgba(255,255,255,0.6)"
+		width:Width,
+		backgroundColor:"#999",
+		marginTop:55,
 	},
 	body:{
 		flexDirection:"column",
 		alignItems:"center",
-		position:"absolute",
-		height:140,
-		top:-140
-	},
-	lines:{
-		flexDirection:"row",
-		width:Width,
-		justifyContent:"space-around",
-		height:80
-	},
-	img:{
-		width:50,
-		height:50
-	},
-	btns:{
-		position:"absolute",
-		top:400
-	},
-	btn:{
-		height:40,
-		backgroundColor:"#999"
-	},
-	text:{
-		color:"#fff"
+		justifyContent:"center",
 	}
 });
 
